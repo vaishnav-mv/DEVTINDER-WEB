525bb441027a0b23bfda3df65e014fc3ce8df2ca
vaishnav-mv <vaishnavam02@gmail.com>
2025-12-05 22:56:31 +0530

    removed default username

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../utils/constants'
 
 const Login = () => {
-    const [emailId,setEmail]=useState("user@gmail.com")
-    const [password,setPassword]=useState("User@123")
+    const [emailId,setEmail]=useState("")
+    const [password,setPassword]=useState("")
     const [error,setError]=useState("")
     const dispatch=useDispatch()
     const navigate=useNavigate()
